7ceec29d08414f18554e2e176c28ca7daf0f80fe
felipebrunet <felipe.brunet@gmail.com>
2022-10-18 00:04:20 -0300

    primer commit

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useEffect } from "react";
+import { tasks as data } from "../data/tasks";
+
+export const TaskContext = createContext();
+
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+  const [counter, setCounter] = useState(data.length);
+
+  function createTask(task) {
+    const newTask = {
+      id: counter,
+      title: task.title,
+      description: task.description,
+    };
+    setTasks([...tasks, newTask]);
+    setCounter(counter + 1);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        createTask,
+        deleteTask,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+}
